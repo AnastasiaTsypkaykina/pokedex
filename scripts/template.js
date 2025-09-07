3beb5d2c03dd76fe32ef1530ee9f2039cf7c450b
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-09-07 12:57:58 +0200

    navigantion buttons added

--- scripts/template.js
+++ scripts/template.js
@@ -41,7 +41,9 @@ function currentPokemonTemp(pokemon, i, species) {
                 <div class="pokeTypes" id="pokeTypes${i}"></div>
 
                 <div class="pokeImg">
+                    <img class="prev-arrow" src="./img/arrow-next.svg" onclick="openPokemonOverlay(${pokemon.id-2})" />
                     <img src="${pokemon.sprites.other["official-artwork"]["front_default"]}" />
+                    <img class="next-arrow"src="./img/arrow-next.svg" onclick="openPokemonOverlay(${pokemon.id})" />
                 </div>            
 
                 <div class="typeImg">
